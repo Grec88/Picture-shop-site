a461547cd390e9d49dbf245165e768f4b4f1c33e
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-03-07 21:45:17 +0300

    Styles from json DB

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,17 +1,31 @@
-export const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles);
+import { getResource } from "../services/requests";
+
+
+export const showMoreStyles = (trigger, wrapper) => {
     const btn = document.querySelector(trigger);
 
-    cards.forEach(card => {
-        card.classList.add('animated', 'fadeInUp');
-    });
+    const createCards = (responses) => {
+        responses.forEach(({src, title, link}) => {
+            let card = document.createElement('div');
 
-    btn.addEventListener('click', () => {
-        cards.forEach(card => {
-            card.classList.remove('hidden-lg', "hidden-md", "hidden-sm", "hidden-xs");
-            card.classList.add("col-sm-3", "col-sm-offset-0", "col-xs-10", "col-xs-offset-1");
+            card.classList.add('animated', 'fadeInUp', "col-sm-3", "col-sm-offset-0", "col-xs-10", "col-xs-offset-1");
+            card.innerHTML = `
+            <div class=styles-block>
+                <img src=${src} alt>
+                <h4>${title}</h4>
+                <a href=${link}>Подробнее</a>
+        </div>
+            `;
+             
+
+            document.querySelector(wrapper).appendChild(card);
         });
+    };
 
-        btn.style.display = 'none';
+    btn.addEventListener('click', (e) => {
+        getResource('http://localhost:3000/styles')
+            .then(res => createCards(res))
+            .catch(error => console.log(error))
+            e.target.remove();
     });
 };
